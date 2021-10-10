b09d7930f749953a1e0d708e71e933fb1c1635b6
Kylian Sauvée <kylian@sauvee.com>
2021-10-10 16:39:39 +0200

    feat: add adjacency matrix

--- src/floyd_warshall_graph.py
+++ src/floyd_warshall_graph.py
@@ -1,10 +1,10 @@
 class floyd_warshall_graph:
-    def __init__(self, nb_nodes, ):
+    def __init__(self, nb_nodes):
         if nb_nodes < 0:
             raise ValueError
         self.__nb_nodes = nb_nodes
         self.__adj_matrix = [[0 for _ in range(self.__nb_nodes)] for _ in range(self.__nb_nodes)]
 
     def get_adj_matrix(self):
-        new_adj_matrix = [line.copy() for line in self.__adj_matrix]
+        new_adj_matrix = [node_neighbors.copy() for node_neighbors in self.__adj_matrix]
         return new_adj_matrix
--- tests/test_floyd_warshall_graph.py
+++ tests/test_floyd_warshall_graph.py
@@ -3,11 +3,30 @@ import pytest
 from src.floyd_warshall_graph import floyd_warshall_graph
 
 
-def test_negative_nodes():
+def test_negative_number_of_nodes():
     with pytest.raises(ValueError):
         floyd_warshall_graph(-3)
 
 
+def test_graph_without_node():
+    graph_test = floyd_warshall_graph(0)
+    assert len(graph_test.get_adj_matrix()) == 0
+
+    for node_neighbors in graph_test.get_adj_matrix():
+        assert len(node_neighbors) == 0
+
+
 def test_graph_with_one_node():
-    graph = floyd_warshall_graph(1)
-    assert len(graph.get_adj_matrix()) == 1
+    graph_test = floyd_warshall_graph(1)
+    assert len(graph_test.get_adj_matrix()) == 1
+
+    for node_neighbors in graph_test.get_adj_matrix():
+        assert len(node_neighbors) == 1
+
+
+def test_graph_with_54_nodes():
+    graph_test = floyd_warshall_graph(54)
+    assert len(graph_test.get_adj_matrix()) == 54
+
+    for node_neighbors in graph_test.get_adj_matrix():
+        assert len(node_neighbors) == 54
