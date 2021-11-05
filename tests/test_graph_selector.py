52d4b807da8478c703e5e68640f9fd8087b40fe2
Kylian Sauvée <kylian@sauvee.com>
2021-11-06 00:59:13 +0100

    test: add and pass test about graph selector with any number

--- tests/test_graph_selector.py
+++ tests/test_graph_selector.py
@@ -4,3 +4,8 @@ from src.graph_selector import GraphSelector
 def test_graph_selector_graph_number_1(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda: "1")
     assert GraphSelector.graph_selector() == "resources/test_graphs/G?-1.txt"
+
+
+def test_graph_selector_graph_with_any_number(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda: "56")
+    assert GraphSelector.graph_selector() == "resources/test_graphs/G?-56.txt"
