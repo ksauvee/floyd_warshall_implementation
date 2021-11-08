ae712c0fbd21c584c297e5be4e769804b0c8034b
Kylian Sauvée <kylian@sauvee.com>
2021-11-08 14:39:59 +0100

    fix: delete '?'

--- tests/test_graph_parser.py
+++ tests/test_graph_parser.py
@@ -5,7 +5,7 @@ from src.graph_parser import GraphParser
 
 def test_translate_file_to_data_0_node():
     graph_test_nb_nodes, graph_test_nb_edges, graph_test_edges = \
-        GraphParser.translate_file_to_data("resources/test_graphs/G?-1.txt")
+        GraphParser.translate_file_to_data("resources/test_graphs/G-1.txt")
     assert graph_test_nb_nodes == 0
     assert graph_test_nb_edges == 0
     assert len(graph_test_edges) == 0
@@ -13,24 +13,15 @@ def test_translate_file_to_data_0_node():
 
 def test_translate_file_to_data_1_node():
     graph_test_nb_nodes, graph_test_nb_edges, graph_test_edges = \
-        GraphParser.translate_file_to_data("resources/test_graphs/G?-2.txt")
+        GraphParser.translate_file_to_data("resources/test_graphs/G-2.txt")
     assert graph_test_nb_nodes == 1
     assert graph_test_nb_edges == 0
     assert len(graph_test_edges) == 0
 
 
-def test_translate_file_to_data_2_nodes():
-    graph_test_nb_nodes, graph_test_nb_edges, graph_test_edges = \
-        GraphParser.translate_file_to_data("resources/test_graphs/G?-3.txt")
-    assert graph_test_nb_nodes == 2
-    assert graph_test_nb_edges == 1
-    assert len(graph_test_edges) == 1
-    assert graph_test_edges == [[0, 1, 1]]
-
-
 def test_translate_file_to_data_some_graph():
     graph_test_nb_nodes, graph_test_nb_edges, graph_test_edges = \
-        GraphParser.translate_file_to_data("resources/test_graphs/G?-4.txt")
+        GraphParser.translate_file_to_data("resources/test_graphs/G-4.txt")
     assert graph_test_nb_nodes == 4
     assert graph_test_nb_edges == 5
     assert len(graph_test_edges) == 5
