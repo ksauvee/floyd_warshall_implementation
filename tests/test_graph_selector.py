3e97a24548810f8b7a7f34cce96abdd1dcd231cb
Kylian Sauvée <kylian@sauvee.com>
2021-12-23 10:53:05 +0100

    test: fix existing tests

--- tests/test_graph_selector.py
+++ tests/test_graph_selector.py
@@ -3,9 +3,9 @@ from src.graph_selector import GraphSelector
 
 def test_graph_selector_graph_number_1(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda: "1")
-    assert GraphSelector.graph_selector() == "resources/graphs_examples/G-1.txt"
+    assert GraphSelector.graph_selector(store_traces=False) == ("resources/test_graphs/G2-1.txt", "")
 
 
 def test_graph_selector_graph_with_any_number(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda: "56")
-    assert GraphSelector.graph_selector() == "resources/graphs_examples/G-56.txt"
+    monkeypatch.setattr('builtins.input', lambda: "12")
+    assert GraphSelector.graph_selector(store_traces=False) == ("resources/test_graphs/G2-12.txt", "")
