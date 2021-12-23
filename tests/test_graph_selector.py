c54ca2b744778fb2838037033f5c5b44439aecf9
Kylian Sauvée <kylian@sauvee.com>
2021-12-23 11:18:45 +0100

    test: add and pass test on graph_selector with store_traces

--- tests/test_graph_selector.py
+++ tests/test_graph_selector.py
@@ -9,3 +9,9 @@ def test_graph_selector_graph_number_1(monkeypatch):
 def test_graph_selector_graph_with_any_number(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda: "12")
     assert GraphSelector.graph_selector(store_traces=False) == ("resources/test_graphs/G2-12.txt", "")
+
+
+def test_graph_selector_when_store_traces(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda: "1")
+    assert GraphSelector.graph_selector(store_traces=True) == \
+           ("resources/test_graphs/G2-1.txt", "resources/execution_traces/G2-trace-1.txt")
