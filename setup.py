a4c94c5c630d3c8250983eaa3e38cdbd56cd387c
Kylian Sauvée <kylian@sauvee.com>
2022-01-02 18:49:48 +0100

    feat: modify project version 0.0.1 -> 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name="floyd_warshall_implementation",
-   version="0.0.1",
+   version="1.0",
    description="A Floyd Warshall Implementation",
    author="Jules Rubin, Oscar Vilmot, Joël Gabbai, Theo Martin, Kylian Sauvée",
    author_email="kylian@sauvee.com",
