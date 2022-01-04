053eb719c8f178f2ab5aa29166c47a9968257626
Kylian Sauvée <kylian@sauvee.com>
2022-01-04 22:02:46 +0100

    test(Graph_selector): improve test coverage (100%)

--- tests/test_graph_selector.py
+++ tests/test_graph_selector.py
@@ -15,3 +15,12 @@ def test_graph_selector_when_store_traces(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda: "1")
     assert GraphSelector.graph_selector(store_traces=True) == \
            ("resources/test_graphs/1.txt", "resources/execution_traces/trace_1.txt")
+
+
+def test_graph_selector_with_invalid_input(mocker, capsys):
+    mocker.patch('builtins.input', side_effect=["17", "3"])
+    GraphSelector.graph_selector(store_traces=True)
+    captured = capsys.readouterr()
+    assert captured.out == "Entrez le numéro du graphe que vous souhaitez tester : Entrez le numéro du graphe que " \
+                           "vous souhaitez tester (Veuillez entrer un nombre entre 1 " \
+                           "et 13 inclus) : "
